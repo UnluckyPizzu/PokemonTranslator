fd32e632b211f725b1182d067d48a185c799845f
Emilio Pizzuto <emilio.pizzuto@esternibisp.com>
2025-02-04 12:10:56 +0100

    Added full coverage of PokemonControllerImpl

--- src/test/java/com/pizzu/rest/PokemonControllerImplTest.java
+++ src/test/java/com/pizzu/rest/PokemonControllerImplTest.java
@@ -2,6 +2,7 @@ package com.pizzu.rest;
 
 import com.pizzu.model.Pokemon;
 import com.pizzu.service.PokemonService;
+import com.pizzu.service.TranslateService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +21,9 @@ public class PokemonControllerImplTest {
 
     @Mock
     private PokemonService pokemonService;
+
+    @Mock
+    private TranslateService translateService;
 
     @Test
     public void givenName_whenGetPokemonInfo_thenReturnResponseSuccessWithPokemon(){
@@ -41,6 +45,32 @@ public class PokemonControllerImplTest {
         Mockito.when(pokemonService.getPokemonTranslatedDescriptionByName(Mockito.anyString())).thenReturn(null);
 
         ResponseEntity<Pokemon> result = pokemonController.getPokemonInfo("test");
+
+        Assertions.assertNull(result.getBody());
+
+        Assertions.assertEquals(result.getStatusCode(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Test
+    public void givenPokemon_whengetPokemonInfoTranslated_thenReturnResponseSuccessWithPokemon(){
+        Pokemon pokemon = new Pokemon();
+        Mockito.when(pokemonService.getPokemonTranslatedDescriptionByName(Mockito.anyString())).thenReturn(pokemon);
+        Mockito.when(translateService.getTranslatePokemon(Mockito.any(Pokemon.class))).thenReturn(pokemon);
+
+
+        ResponseEntity<Pokemon> result = pokemonController.getPokemonInfoTranslated("test");
+
+        Assertions.assertNotNull(result.getBody());
+
+        Assertions.assertEquals(result.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void givenNull_whengetPokemonInfoTranslated_thenReturnResponseFailureWithNull(){
+        Mockito.when(pokemonService.getPokemonTranslatedDescriptionByName(Mockito.anyString())).thenReturn(null);
+        Mockito.when(translateService.getTranslatePokemon(Mockito.any())).thenReturn(null);
+
+        ResponseEntity<Pokemon> result = pokemonController.getPokemonInfoTranslated("test");
 
         Assertions.assertNull(result.getBody());
 
